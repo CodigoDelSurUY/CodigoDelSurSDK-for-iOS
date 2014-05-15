81fd76fe51b1e748399219ef503811ac4cf79cd1
Nicolas Jakubowski <njakubowski@codigodelsur.com>
2014-05-15 16:40:31 -0300

    Fixed typo in main header file

--- CodigoDelSurSDK/CodigoDelSurSDK.h
+++ CodigoDelSurSDK/CodigoDelSurSDK.h
@@ -7,7 +7,7 @@
 //
 
 #if !__has_feature(objc_arc)
-#error SVProgressHUD is ARC only. Either turn on ARC for the project or use -fobjc-arc flag
+#error CodigoDelSurSDK is ARC only. Either turn on ARC for the project or use -fobjc-arc flag
 #endif
 
 /**
